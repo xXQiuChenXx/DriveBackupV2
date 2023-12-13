4f96dec110ccabc34a0be0cd9b4a27243dafa94e
秋辰 <51996408+xXQiuChenXx@users.noreply.github.com>
2023-12-14 00:18:59 +0800

    Update Authenticator.java

--- DriveBackup/src/main/java/ratismal/drivebackup/uploaders/Authenticator.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/Authenticator.java
@@ -44,7 +44,8 @@ public class Authenticator {
 
     public enum AuthenticationProvider {
         GOOGLE_DRIVE("Google Drive", "googledrive", "/GoogleDriveCredential.json", "qWd2xXC/ORzdZvUotXoWhHC0POkMNuO/xuwcKWc9s1LLodayZXvkdKimmpOQqWYS6I+qGSrYNb8UCJWMhrgDXhIWEbDvytkQTwq+uNcnfw8=", "pasQz0KvtyC7o6CrlLPSMVV9Y0RMX76cXzsAbBoCBxI="),
-        ONEDRIVE("OneDrive", "onedrive", "/OneDriveCredential.json", "Ktj7Jd1h0oYNVicuyTBk5fU+gHS+QYReZxZKNZNO9CDxxHaf8bXlw0SKO9jnwc81", ""),
+        ONEDRIVE("OneDrive", "onedrive", "/OneDriveCredential.json", "FFJyhFHiuYvfVQZCcuVW7ls9lsE4IodZwo/a0ZtMYo++UyV/YLFEUjcqwwzAv2qb", ""),
+        // ONEDRIVE("OneDrive", "onedrive", "/OneDriveCredential.json", "Ktj7Jd1h0oYNVicuyTBk5fU+gHS+QYReZxZKNZNO9CDxxHaf8bXlw0SKO9jnwc81", ""),
         DROPBOX("Dropbox", "dropbox", "/DropboxCredential.json", "OSpqXymVUFSRnANAmj2DTA==", "4MrYNbN0I6J/fsAFeF00GQ==");
 
         private final String name;
